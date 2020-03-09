9d8cbb8da81c71e72fd130426a880b7541e2d98a
Sina <sina202@gmail.com>
2020-03-08 22:14:58 -0700

    add styles to loader

---
+++ js/components/Loader/styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  loader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+});
+export default styles;
