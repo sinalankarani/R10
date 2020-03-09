6ca597c4f5460465f137fc293ee60e7f1ea6f75c
Sina <sina202@gmail.com>
2020-03-08 22:10:04 -0700

    change navigation imports

--- js/navigation/index.js
+++ js/navigation/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import BottomTabNavScreens from './BottomTabNav';
+import MainScreens from './Navigation';
 import SpeakerModal from '../screens/Speaker';
 
 const RootStack = createStackNavigator();
 
 const RootStackScreens = props => (
   <RootStack.Navigator headerMode="none" mode="modal">
-    <RootStack.Screen name="BottomTab" component={BottomTabNavScreens} />
+    <RootStack.Screen name="BottomTab" component={MainScreens} />
     <RootStack.Screen name="Speaker" component={SpeakerModal} />
   </RootStack.Navigator>
 );
